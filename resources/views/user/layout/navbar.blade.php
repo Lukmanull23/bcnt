2f0a3c0e8fca625a63b77aca92c2c5386c2c5fd3
miatripra <you@example.com>
2022-12-28 12:30:55 +0700

    first commit

--- resources/views/user/layout/navbar.blade.php
+++ resources/views/user/layout/navbar.blade.php
@@ -26,7 +26,7 @@
                     </ul>
 
                     <ul class="navbar-nav ms-auto">
-                        <button class="btn btn-outline-dark" type="submit">
+                        <button class="btn btn-outline-dark" type="submit" herf="/cart">
                             <i class="bi-cart-fill me-1"></i>
                             Cart
                             <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
