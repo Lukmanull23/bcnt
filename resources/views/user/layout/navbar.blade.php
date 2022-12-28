f90bae47066d57df38ddfce10ce69308c506045c
unknown <lukmanullh23@upi.edu>
2022-12-28 12:01:07 +0700

    memperbaharui tampilan treatment

--- resources/views/user/layout/navbar.blade.php
+++ resources/views/user/layout/navbar.blade.php
@@ -11,7 +11,7 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="/home">Home</a></li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Treatment</a>
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="/treatment" role="button" data-bs-toggle="dropdown" aria-expanded="false">Treatment</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="/treatment">All Products</a></li>
                                 <li><hr class="dropdown-divider" /></li>
